4ef7e4d4545051ce380aa4fbd9589694130eccb1
jinyoung0305 <pohanin1220@gmail.com>
2019-05-13 13:03:15 +0900

    new-feature 브랜치에 코드추가

--- SouceTreeTest.c
+++ SouceTreeTest.c
@@ -2,7 +2,14 @@
 
 int main(void)
 {
+        int i = 0;
+
         printf("first OSS code\n");
         printf("second OSS code\n");
+
+        for(int i=0; i<10; i++)
+        {
+                print("Test 브랜치/n");
+        }
         return 0;
 }
