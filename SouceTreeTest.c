330a26005be94ed168e3f031e456ad5af89958db
jinyoung0305 <pohanin1220@gmail.com>
2019-05-13 13:01:33 +0900

    Revert "no message"
    This reverts commit ea75d3a6097b34fdbc0c02479c84ff87a3047fa1.

--- SouceTreeTest.c
+++ SouceTreeTest.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 
 int main(void)
-{       
-        int i = 0;
-
+{
         printf("first OSS code\n");
         printf("second OSS code\n");
-
-        for (int i=0; i<10; i++){
-                printf("Test 브랜치/n");
-        }
         return 0;
 }
